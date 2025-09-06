e08a3c9d8a9d601882b9d45564bc3eb1b7eec440
SHIVAM TRIPATHI <142592765+Shivam123Tripathi@users.noreply.github.com>
2025-09-06 15:01:54 +0000

    c

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -6,86 +6,97 @@
 //  tinderUser.isLoggedIn = false
 // console.log(tinderUser);
 
-const regularUser = {
-    email: "sum@gmail.com",
-    fullname: {
-        userfullname: {
-            firstname: "shivam",
-            lastname:"tripathi"
+// const regularUser = {
+//     email: "sum@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "shivam",
+//             lastname:"tripathi"
 
-        }
-    }
-}
-console.log(regularUser.fullname.userfullname.firstname) //? is used if there is no value nothing will happen no need to use if else
-
-
-
-    const obj1 = {1: "a", 2: "b"}
-    const obj2 = {3: "a", 4: "b"}
-    const obj4 = {5: "a", 6: "b"}
-
-    //const obj3 = {obj1, obj2}
-    //const obj3 = Object.assign({}, obj1, obj2, obj4)
-    //console.log(obj3); //{} empty object is given to it and ob1,obj2 these are source.
-
-    const obj3 = {...obj1,...obj2} //...(dot dot dot means spread this ... is used mostly.
-    console.log(obj3);
-    
-    const users = [
-        {
-            id: 1,
-            email: "h@gmail.com"
-        }
-    ]
-
-    users[1].email
-    console.log(tinderUser);
-    
-// console.log(Object.keys(tinderUser)); //very important
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
-
-// console.log(tinderUser.hasOwnPropery('fisLoggedIn') );
-
-//destructring of arry and object both happened in both
-
-const course = {
-    coursename: "js ",
-    price: "999",
-    courseInstructor: "hitesh"
-}
-//course.courseInstructor
-
-const {courseInstructor: instructor} = course
-
-console.log(courseInstructor);
-//now we shorten the name courseInstructor to instructor
-
-// const navbar = (props.company) => {
-
+//         }
+//     }
 // }
-
-// navbar (company = "shivam") 
-    //above is destructuring of objects
+// console.log(regularUser.fullname.userfullname.firstname) //? is used if there is no value nothing will happen no need to use if else
 
 
-    //api => apna kaam kisi or ke sir p example: waiter at restaurant
 
-    //json is also object only written in curly braces just like curly brackets
-    // value must be in strings as shown below
+//     const obj1 = {1: "a", 2: "b"}
+//     const obj2 = {3: "a", 4: "b"}
+//     const obj4 = {5: "a", 6: "b"}
 
-    // {
-    //     "name": "shivam",
-    //     "coursename": "js",
-    //     "price": "free"
-    // }
+//     //const obj3 = {obj1, obj2}
+//     //const obj3 = Object.assign({}, obj1, obj2, obj4)
+//     //console.log(obj3); //{} empty object is given to it and ob1,obj2 these are source.
 
-    //sometimes apis also is in array format as show below
-
-    [
-        {},
-        {},
-        {}
-    ]
-
+//     const obj3 = {...obj1,...obj2} //...(dot dot dot means spread this ... is used mostly.
+//     console.log(obj3);
     
+//     const users = [
+//         {
+//             id: 1,
+//             email: "h@gmail.com"
+//         }
+//     ]
+
+//     users[1].email
+//     console.log(tinderUser);
+    
+// // console.log(Object.keys(tinderUser)); //very important
+// // console.log(Object.values(tinderUser));
+// // console.log(Object.entries(tinderUser));
+
+// // console.log(tinderUser.hasOwnPropery('fisLoggedIn') );
+
+// //destructring of arry and object both happened in both
+
+// const course = {
+//     coursename: "js ",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+// //course.courseInstructor
+
+// const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+// //now we shorten the name courseInstructor to instructor
+
+// // const navbar = (props.company) => {
+
+// // }
+
+// // navbar (company = "shivam") 
+//     //above is destructuring of objects
+
+
+//     //api => apna kaam kisi or ke sir p example: waiter at restaurant
+
+//     //json is also object only written in curly braces just like curly brackets
+//     // value must be in strings as shown below
+
+//     // {
+//     //     "name": "shivam",
+//     //     "coursename": "js",
+//     //     "price": "free"
+//     // }
+
+//     //sometimes apis also is in array format as show below
+
+//     [
+//         {},
+//         {},
+//         {}
+//     ]
+
+//destructuring of an object 
+    const course = {
+        coursename: "javascript",
+        price: "999",
+        courseInstructor: "hitesh"
+    }
+    course.courseInstructor
+
+const {coursename: name} = course //in this line we wrote :name after course name so now it has short name and we can call coursename with name only
+console.log(name); 
+
+const navbar = ()
