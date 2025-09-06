b43dd856007b085ac48bebaeffdacba618a8778c
SHIVAM TRIPATHI <142592765+Shivam123Tripathi@users.noreply.github.com>
2025-09-06 15:36:17 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,45 @@
+function sayMyName (){ //syntax of function
+    console.log("s");
+    console.log("h");
+    console.log("i");
+    console.log("v");
+    console.log("a");
+    console.log("m");
+}
+
+//sayMyName ()
+
+function addTwoNumbers(number1,number2){ //these are parameters 
+   console.log( number1 + number2);
+   
+}
+//addTwoNumbers(3, "null") //these are arguments
+
+
+function addTwoNumbers(number1,number2){
+
+    //let result = number1 + number2
+    //return result
+    return number1 + number2
+    console.log("shivam"); //this line will never execute because above return is written return k baad kuch bhi print ni hota
+    
+}
+
+const result = addTwoNumbers(3,5)
+
+//console.log("Result: ", result); //result: undefined
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+  
+
+//console.log(loginUserMesage("Shivam"))
+//console.log(loginUserMessage());
+
+
+//return likhne se output print ni hota return ke baad console.log karna padta h for output
