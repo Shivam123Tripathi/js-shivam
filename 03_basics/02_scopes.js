661b10f773f67fbb57623517a033263432a834a3
SHIVAM TRIPATHI <142592765+Shivam123Tripathi@users.noreply.github.com>
2025-09-18 14:28:52 +0000

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,5 +1,4 @@
 
-
 let a = 300
 let b = 20
 var c = 30
@@ -9,14 +8,61 @@ var c = 30
 if(true){
     let a = 10
     let b = 20
-    console.log("INNER:", a);
+    //console.log("INNER:", a);
     
     //var c = 30 //var is avoided to use
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
 
 //scope ke andar ki value bahar nahi jani chahiye
+
+
+//nested scope
+function one (){
+    const username = "shivam"
+
+    function two(){
+    const website = "github"
+   // console.log(username);
+    
+}
+//console.log(website);
+two()
+}
+one()
+
+//uppar two jo h one se maang skta h par one bada h or two chhota h to one apne chhote se ni maang sakta :p
+
+
+if (true) {
+    const username = "shivam"
+    if (username == "shivam") {
+        const website = "github"
+        //console.log(username + website);
+        
+        
+    }
+   // console.log(website);
+    
+}
+//console.log(username);
+
+
+
+//+++++++++++++++++++++ interesting +++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
+
+//dono tarah se function bn sakte h dono use hote h 
